0866006ad979b0b99000ce222a1ef58369e35550
Sagar Dhadke <peaceunique2003@gmail.com>
2023-08-26 21:42:22 +0530

    New Files Added

---
+++ wp-content/plugins/wordpress-seo-premium/vendor/yoast/wordpress-seo/vendor/composer/installed.php
@@ -0,0 +1,44 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '20.13',
+        'version' => '20.13.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0d694150782e403871d95fb2870a84dde440c734',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => '20.13',
+            'version' => '20.13.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0d694150782e403871d95fb2870a84dde440c734',
+            'dev_requirement' => false,
+        ),
+    ),
+);
